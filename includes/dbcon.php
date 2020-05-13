467a45a1200995eee92ce130fd96ab0191c18696
IdriziJugerta <jugerta.idrizi@fshnstudent.info>
2020-05-13 19:27:48 +0200

    update front

--- includes/dbcon.php
+++ includes/dbcon.php
@@ -1,9 +1,9 @@
 <?php 
-$host="localhost";
+$host="127.0.0.1";
 $username="root";
 $password="";
 $db_name="projekt_web";
 //krijojme lidhjen me databazen dhe e kontrollojme per gabime
-$conn = new mysqli($host, $username, $password,$db_name) or die (mysql_error($conn));
+$conn = new mysqli($host, $username, $password,$db_name,'3307') or die (mysql_error($conn));
 
-?>
+?> 
--- shfaqjet.php
+++ shfaqjet.php
@@ -1,23 +1,42 @@
 <?php
   include 'includes/header.php';
+  
   require_once 'includes/dbcon.php';
   $sql="SELECT * FROM shfaqje";
     $rez=mysqli_query($conn,$sql);
 ?>
 
 <!DOCTYPE html>
-    <body>
-   
+<head>
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.column1 {
+  float: left;
+  width: 33.33%;
+  padding: 5px;
+}
+
+/* Clearfix (clear floats) */
+.row1::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+</style>
+</head>
+ <body>
+    <div class = "row1">
         <?php
-//do te afishohen te gjitha shfaqjet me imazhin dhe emrin te cilat jane te klikueshme dhe te cojne ne faqen perkatese te shfaqjes
-            while($row=mysqli_fetch_assoc($rez)){ ?>
-            <div>
-            <a href='shfaqja.php?shfaqje_id=<?php echo $row['shfaqje_id'];?>'>
-            <p><img src='images/<?php echo $row['image']?>'></p>
-            <p> <?php echo $row['shfaqje_emer'] ?></p>
-            </a>
+                //do te afishohen te gjitha shfaqjet me imazhin dhe emrin te cilat jane te klikueshme dhe te cojne ne faqen perkatese te shfaqjes
+          while($row=mysqli_fetch_assoc($rez)){ ?>
+            <div class = "column1" > 
+              <a href='shfaqja.php?shfaqje_id=<?php echo $row['shfaqje_id'];?>'> <p><img src='images/<?php echo $row['image']?>'style= "width:50%  "></p><p> <?php echo $row['shfaqje_emer'] ?></p> 
             </div>
-           <?php } ?>
-               
+         <?php } ?>
+     </div>   
      </body>
  </html>
+   

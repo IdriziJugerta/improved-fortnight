c5713b36a10ab53a15dcf42639f765bdc69cc06b
JoraldaM <64036982+JoraldaM@users.noreply.github.com>
2020-05-21 01:03:46 +0200

    Add files via upload

---
+++ edit_teater.php
@@ -0,0 +1,74 @@
+﻿<?php
+include_once("includes/dbcon.php");
+// Kontrollon nese forma me te dhenat e update-uara eshte derguar
+if(isset($_POST['Edit']))
+{
+	$teater_id = $_POST['teater_id'];
+
+		$teater = $_POST['teater_emer'];
+		$adrese = $_POST['adrese'];
+        $email= $_POST['email'];
+        $tel = $_POST['tel'];
+
+
+    // update-on te dhenat e teatrit ne tabele bazuar ne id
+	$result = mysqli_query($conn, "UPDATE teater SET teater_emer='$teater',adrese='$adrese',email='$email',tel='$tel' WHERE teater_id=$teater_id");
+    echo"Teatri u shtua me sukses!";
+
+	// kthene ne faqen me te dhenat e update-uara
+	header("Location: teaterCRUD.php");
+}
+?>
+<?php
+//shfaq te dhenat bazuar ne id
+//merr id e teatrit
+$teater_id = $_GET['teater_id'];
+
+// merr te dhenat e teatrit ne baze te id
+$result = mysqli_query($conn, "SELECT * FROM teater WHERE teater_id=$teater_id");
+
+while($teater_data = mysqli_fetch_array($result))
+{
+	$teater = $teater_data['teater_emer'];
+    $adrese = $teater_data['adrese'];
+    $email = $teater_data['email'];
+    $tel = $teater_data['tel'];
+   
+}
+?>
+<html>
+<head>
+    <title>Edit Theatre Data</title>
+    <link rel="stylesheet" type="text/css" href="adminStyle.css" />
+</head>
+
+<body>
+    <p align='center'><a href="teaterCRUD.php" class="link" align="center">Home</a></p>
+    <br /><br />
+
+    <form name="frm" method="post" action="edit_teater.php" >
+        <table border="1" cellpadding="10" cellspacing="3" width="500" align="center">
+         
+            <tr>
+                <td><label>Emer Teatri</label></td>
+                <td> <input type="text" name="teater_emer" placeholder="Emer Teatri" value=<?php echo $teater;?>></td>
+            </tr>
+            <tr>
+                <td> <label>Adrese</label></td>
+                <td><input type="text" name="adrese" placeholder="Adrese" value=<?php echo $adrese;?>></td>
+            </tr>
+            <tr>
+                <td> <label>Email</label></td>
+                <td><input type="email" name="email" placeholder="Email" value=<?php echo $email;?>></td>
+            </tr>
+            <tr>
+                <td><label>Nr. Telefoni</label></td>
+                <td><input type="number" name="tel" placeholder="Nr telefoni"  pattern="^(?=.*06[7-9]+.*)(?=.*[0-9]+.*)[0-9]{7,}$" value=<?php echo $tel;?>></td>
+            </tr>
+            <tr>
+                <td><input type="hidden" name="teater_id" value=<?php echo $_GET['teater_id'];?>></td>
+                <td colspan="2" align="center"><input type="submit" name="Edit" value="Edit" class="crudBtn"></td>
+        </table>
+    </form>
+</body>
+</html>
---
+++ teaterCRUD.php
@@ -0,0 +1,50 @@
+﻿<?php
+//perfshin file qe ben lidhjen me databazen
+include_once("includes/dbcon.php");
+//merr te dhenat e teatrit nga databaza
+$sql = "SELECT * FROM teater ORDER BY teater_id DESC";
+$result = mysqli_query($conn,$sql) or die(mysqli_error($conn));
+?>
+<html>
+<head>
+    <title>Theatres</title>
+    <link rel="stylesheet" type="text/css" href="adminStyle.css" />
+</head>
+<body>
+    </br>
+    </br>
+    <a href='admin.php' class='link' align='center'>Admin Page</a>
+    <form name="frm" method="post" action="">
+        <table border="1" cellpadding="10" cellspacing="3" width="500" align="center" style="padding-top:25px;">
+            <tr>
+                <th>Emer Teatri</th>
+                <th>Adrese</th>
+                <th>Email</th>
+                <th>Nr Telefoni</th>
+                <th>CRUD Actions</th>
+            </tr>
+            <?php
+            while($teater_data = mysqli_fetch_array($result)) {
+            echo "
+            <tr>
+                ";
+                echo "
+                <td>".$teater_data["teater_emer"]."</td>";
+                echo "
+                <td>".$teater_data["adrese"]."</td>";
+                echo "
+                <td>".$teater_data["email"]."</td>";
+                echo  "
+                <td>".$teater_data["tel"]."</td>";
+                echo "
+                <td><a href='edit_teater.php?teater_id=$teater_data[teater_id]' class='link'>Edit</a> | <a href='delete_teater.php?teater_id=$teater_data[teater_id]' class='link'>Delete</a></td>
+            </tr>";
+
+            }
+            ?>
+
+        </table>
+        <div align="center" style="padding-top:25px;"><a href="add_teater.php" class="link"> Shto teater</a></div>
+    </form>
+</body>
+</html>

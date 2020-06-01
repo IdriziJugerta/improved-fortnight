306ca68ba73703b7fbbb007f9577320e353388e4
estelDona <51865091+estelDona@users.noreply.github.com>
2020-06-01 14:33:50 +0200

    Update salle.php

--- salle.php
+++ salle.php
@@ -18,6 +18,7 @@ $result = mysqli_query($conn,$sql) or die(mysqli_error($conn));
 <form name="frmShow" method="post" action="">
 <table border="1" cellpadding="10" cellspacing="3" width="500" align="center" style="padding-top:25px;">
 <tr>
+<th>Id Salle</th>
 <th>Id Teatrit</th>
 <th>Emri Salles</th>
 <th>Vende</th>
@@ -26,6 +27,7 @@ $result = mysqli_query($conn,$sql) or die(mysqli_error($conn));
 <?php
 while($salle_data = mysqli_fetch_array($result)) {
      echo "<tr>";
+     echo "<td>".$salle_data["salle_id"]."</td>";
      echo "<td>".$salle_data["t_id"]."</td>";
      echo "<td>".$salle_data["emer_salle"]."</td>";
      echo "<td>".$salle_data["seats"]."</td>";
